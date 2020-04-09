70359cf9f7ef330a331799219d771c02903a0a69
Matthew Sullivan <contact@matthewsullivan.media>
2020-04-09 08:54:57 -0400

    Remove Postgres references

--- config/lib/pg/pg.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const {extend} = require('pg-extra');
-
-const config = require(path.resolve('./config/env/default'));
-
-const pg = extend(require('pg'));
-const pool = new pg.Pool(config.db);
-
-module.exports = {pool};
